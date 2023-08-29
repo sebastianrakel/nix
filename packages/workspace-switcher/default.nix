4e141a1d7eead81a8848a9f2ef9955cd502dbd68
Sebastian Rakel <sebastian@devunit.eu>
2023-08-29 14:50:27 +0200

    Update workspaceswitcher

--- packages/workspace-switcher/default.nix
+++ packages/workspace-switcher/default.nix
@@ -1,13 +1,13 @@
-{ pkgs }:
-pkgs.buildGoModule {
+{ lib, pkgs }:
+pkgs.buildGoModule rec {
   pname = "workspace-switcher";
-  version = "0.0.1";
+  version = "0.1.0";
 
   src = pkgs.fetchFromGitHub {
     owner = "sebastianrakel";
     repo = "workspace-switcher";
-    rev = "main";
-    hash = "sha256-qWRXqpHstkeCURckPJQMV4SSXHkzOUgmYnEQioFiCiE=";
+    rev = "v${version}";
+    hash = "sha256-NlKvgx7b0e2LQd9PzsvBOEFYQs7lMzw0TpYtRfdrmwY=";
   };
 
   vendorHash = "sha256-zZ/Psy/cYir4Vsccew2DqqBx2uznQk5bQ7dmWw5s2aI=";

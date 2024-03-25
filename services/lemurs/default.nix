6036faa4f80f4566116035870de936ee62e1b14c
Sebastian Rakel <sebastian@devunit.eu>
2024-03-25 15:21:24 +0100

    Add lemurs config

--- services/lemurs/default.nix
+++ services/lemurs/default.nix
@@ -3,58 +3,102 @@
 with lib;
 
 let
-  cfg = dmcfg.lemurs;
-  dmcfg = config.services.xserver.displayManager;
   xcfg = config.services.xserver;
+  dmcfg = xcfg.displayManager;
+  cfg = dmcfg.lemurs;
+  xEnv = config.systemd.services.display-manager.environment;
+  sessionData = dmcfg.sessionData;
+
   mk_save = cfg.defaultUser != null;
+  tty = "tty${toString xcfg.tty}";
+in {
+  meta.maintainers = with maintainers; [ sebastianrakel ];
 
-  lemursPkg = pkgs.lemurs;
-in
-
-{
   options = {
     services.xserver.displayManager.lemurs = {
-      enable = mkOption {
-        type = types.bool;
-        default = false;
-        description = ''
-          Whether to enable Lemurs as the display manager.
-        '';
-      };
+      enable = mkEnableOption "lemurs as the display manager.";
 
       package = mkOption {
-        default = lemursPkg;
-        description = "lemurs package to use.";
+        type = types.package;
+        default = pkgs.lemurs;
+        defaultText = literalExpression "pkgs.lemurs";
       };
     };
-
   };
 
-
-  ###### implementation
-
   config = mkIf cfg.enable {
-    services.xserver = {
-      displayManager.job.execCmd = "exec ${lemursPkg}/bin/lemurs";
-      displayManager.lightdm.enable = lib.mkForce false;
-    };
 
-    systemd.services.display-manager.after = [
-      "rc-local.service"
-      "systemd-machined.service"
-      "systemd-user-sessions.service"
-      "getty@tty2.service"
-      "user.slice"
+    assertions = [
+      {
+        assertion = xcfg.enable;
+        message = ''
+          lemurs requires services.xserver.enable to be true
+        '';
+      }
     ];
 
-    systemd.services.display-manager.serviceConfig = {
-      Type = "idle";
-      After = ["systemd-user-sessions.service" "plymouth-quit-wait.service" "getty@tty${toString xcfg.tty}.service"];
-      StandardInput = "tty";
-      ExecStart = "${lemursPkg}/bin/lemurs";
-      TTYPath = "/dev/tty${toString xcfg.tty}";
-      TTYReset = true;
-      TTYVHangup = true;
+    environment.systemPackages = [ cfg.package ];
+    environment.etc."lemurs/config.toml" = {
+      mode = "0444";
+      source = ./config.toml;
     };
+
+    environment.etc."lemurs/wms/hlwm" = {
+      mode = "0755";
+      text = ''
+        #! /bin/sh
+        exec ${pkgs.herbstluftwm}/bin/herbstluftwm
+      '';
+    };
+
+    services.xserver.displayManager.lightdm.enable = false;
+
+    security.pam.services.lemurs.text = ''
+        auth       substack     login
+        account    include      login
+        password   substack     login
+        session    include      login
+    '';
+
+    services.xserver.displayManager.job.execCmd = ''
+      exec ${cfg.package}/bin/lemurs
+    '';
+
+    systemd.services."autovt@${tty}".enable = false;
+
+    systemd.services.lemurs = {
+      unitConfig = {
+        Wants= [
+          "systemd-user-sessions.service"
+        ];
+        After = [
+          "systemd-user-sessions.service"
+          "plymouth-quit-wait.service"
+          "getty@${tty}.service"
+        ];
+      };
+
+      serviceConfig = {
+        Type = "idle";
+        ExecStart = "${cfg.package}/bin/lemurs";
+        StandardInput = "tty";
+        TTYPath = "/dev/${tty}";
+        TTYReset = "yes";
+        TTYVHangup = "yes";
+      };
+
+      restartIfChanged = false;
+
+      wantedBy = [ "graphical.target" ];
+    };
+
+    systemd.defaultUnit = "graphical.target";
+
+    users.users.lemurs = {
+      isSystemUser = true;
+      group = "lemurs";
+    };
+
+    users.groups.lemurs = {};
   };
 }

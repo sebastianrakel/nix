8b193c783e655e43cc16ae36fa8b7335520c6d4d
Sebastian Rakel <sebastian@devunit.eu>
2024-07-18 01:00:03 +0200

    add emacs init el

---
+++ users/sebastian/emacs/early-init.el
@@ -0,0 +1,35 @@
+(add-hook 'emacs-startup-hook
+          (lambda ()
+            (message "Loaded Emacs in %.03fs"
+                     (float-time (time-subtract after-init-time before-init-time)))))
+
+(let ((normal-gc-cons-threshold gc-cons-threshold)
+      (normal-gc-cons-percentage gc-cons-percentage)
+      (normal-file-name-handler-alist file-name-handler-alist)
+      (init-gc-cons-threshold most-positive-fixnum)
+      (init-gc-cons-percentage 0.6))
+  (setq gc-cons-threshold init-gc-cons-threshold
+        gc-cons-percentage init-gc-cons-percentage
+        file-name-handler-alist nil)
+  (add-hook 'after-init-hook
+            `(lambda ()
+               (setq gc-cons-threshold ,normal-gc-cons-threshold
+                     gc-cons-percentage ,normal-gc-cons-percentage
+                     file-name-handler-alist ',normal-file-name-handler-alist))))
+
+(setq inhibit-startup-echo-area-message t)
+(setq inhibit-startup-screen t)
+(setq initial-scratch-message nil)
+
+(setq use-dialog-box nil)
+(push '(undecorated . t) default-frame-alist)
+(push '(menu-bar-lines . 0) default-frame-alist)
+(push '(tool-bar-lines . 0) default-frame-alist)
+(push '(vertical-scroll-bars) default-frame-alist)
+
+(setq frame-inhibit-implied-resize t)
+(defvar global-text-scale-adjust-resizes-frames t)
+
+(push '(drag-internal-border . t) default-frame-alist)
+
+(advice-add #'x-apply-session-resources :override #'ignore)

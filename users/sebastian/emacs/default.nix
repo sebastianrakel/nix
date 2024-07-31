2d4107c33c8f0a0577b630ee00f63fb5bb8d5230
Sebastian Rakel <sebastian@devunit.eu>
2024-07-31 08:55:31 +0200

    Add emacs snippets

--- users/sebastian/emacs/default.nix
+++ users/sebastian/emacs/default.nix
@@ -5,9 +5,14 @@
     package = unstable.emacs;
   };
 
+  # TODO: use variable for nix repo location
   home.file = {
     ".emacs.d/init.el".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.nix/users/sebastian/emacs/init.el";
     ".emacs.d/early-init.el".source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.nix/users/sebastian/emacs/early-init.el";
+    ".emacs.d/snippets" = {
+      source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/.nix/users/sebastian/emacs/snippets";
+      recursive = true;
+    };
     ".emacs.d/themes/base16-${config.themes.base16}-theme.el".source = config.themes.base16.emacs;
     ".emacs.d/selected-theme.el".text = ''
       (defvar current-theme 'base16-${config.themes.base16})
